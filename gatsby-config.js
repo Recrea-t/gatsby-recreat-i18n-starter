fb7ea3b5c1b1dd7cc71d9f3919f3e771321857d1
David Gay i Tello <david.gay.tello@protonmail.com>
2021-02-25 16:24:23 +0100

    adding netlify cms

--- gatsby-config.js
+++ gatsby-config.js
@@ -119,6 +119,7 @@ module.exports = {
         metricoolId: process.env.METRICOOL_ID,
       },
     },
+    "gatsby-plugin-netlify-cms",
     {
       resolve: "gatsby-plugin-webpack-bundle-analyser-v2",
       options: {
