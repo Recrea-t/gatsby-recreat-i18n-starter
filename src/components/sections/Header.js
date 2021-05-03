18d870a92f66d39cc6a55a4c306f195b38979bf8
David Gay i Tello <david.gay.tello@protonmail.com>
2021-05-03 10:43:40 +0200

    fix(header): do not break for mobile version

--- src/components/sections/Header.js
+++ src/components/sections/Header.js
@@ -4,7 +4,7 @@ import useTranslations from "../useTranslations"
 
 import { Link as GatsbyLink } from "gatsby"
 import { StaticImage } from "gatsby-plugin-image"
-import { Flex } from "@chakra-ui/react"
+import { Flex, Box } from "@chakra-ui/react"
 
 import NavLink from "../ui/NavLink"
 import ToggleMenu from "../ui/ToggleMenu"
@@ -18,59 +18,65 @@ const Header = () => {
   const toggleMenu = () => setShow(!show)
 
   return (
-    <Flex
+    <Box
       as="nav"
-      h={show ? "auto" : "100px"}
       w="full"
+      h={show ? "auto" : "100px"}
       pos="fixed"
       top="0"
       right="0"
       left="0"
       zIndex="1"
-      align="center"
-      justify="space-between"
-      p={4}
-      mx="auto"
       bg="white"
       color="mangoTango.500"
       borderBottom="1px"
       borderBottomColor="mangoTango.500"
-      wrap="wrap"
     >
-      <LocalizedLink to="/" title={home} as={GatsbyLink}>
-        <StaticImage
-          src="../../images/LogoRecreat.png"
-          alt="Recrea't"
-          loading="eager"
-          layout="fixed"
-          placeholder="tracedSVG"
-          width={200}
-        />
-      </LocalizedLink>
-
-      <Languages />
-
-      <ToggleMenu show={show} toggleMenu={toggleMenu}>
-        {menuItems.map((menu, index) => (
-          <NavLink key={index} to={menu.link} onClick={toggleMenu}>
-            {menu.name}
-          </NavLink>
-        ))}
-      </ToggleMenu>
-
       <Flex
-        display={{ base: "none", md: "inherit" }}
+        h="full"
+        w="full"
+        maxW="1200px"
         align="center"
-        direction="row"
-        justify={{ md: "space-between", lg: "flex-end" }}
+        justify="space-between"
+        mx="auto"
+        p={4}
+        wrap="wrap"
       >
-        {menuItems.map((menu, index) => (
-          <NavLink key={index} to={menu.link}>
-            {menu.name}
-          </NavLink>
-        ))}
+        <LocalizedLink to="/" title={home} as={GatsbyLink}>
+          <StaticImage
+            src="../../images/LogoRecreat.png"
+            alt="Recrea't"
+            loading="eager"
+            layout="fixed"
+            placeholder="tracedSVG"
+            width={200}
+          />
+        </LocalizedLink>
+
+        <ToggleMenu show={show} toggleMenu={toggleMenu}>
+          {menuItems.map((menu, index) => (
+            <NavLink key={index} to={menu.link} onClick={toggleMenu}>
+              {menu.name}
+            </NavLink>
+          ))}
+          <Languages />
+        </ToggleMenu>
+
+        <Flex
+          display={{ base: "none", md: "inherit" }}
+          align="center"
+          direction="row"
+          justify={{ md: "space-between", lg: "flex-end" }}
+        >
+          {menuItems.map((menu, index) => (
+            <NavLink key={index} to={menu.link}>
+              {menu.name}
+            </NavLink>
+          ))}
+          <Languages />
+        </Flex>
       </Flex>
-    </Flex>
+    </Box>
   )
 }
 
